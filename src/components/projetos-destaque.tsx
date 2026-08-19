1e82e4f5912064c5d61756220069605e045c359c
Samelafarias <124313487+Samelafarias@users.noreply.github.com>
2026-08-19 10:32:07 -0300

    feat: ajustes no campo de projetos em destaque

--- src/components/projetos-destaque.tsx
+++ src/components/projetos-destaque.tsx
@@ -22,8 +22,8 @@ const projectsData = [
     subtitle: "Sistema de agendamento para barbearias",
     description: "Sistema moderno de gestão de atendimentos em barbearias.",
     image: "/f2.png",
-    link: "#",
-    features: ["Agendamento em Tempo Real", "Painel Administrativo"],
+    link: "https://pointdobarbeiro.com.br/cliente/servicos/point-do-barbeiro",
+    features: ["Agendamento em Tempo Real", "Painel Administrativo", "Versatilidade", "Acesse de Qualque Lugar, Celular ou PC"],
     techStack: ["NextJs", "Tailwind", "Python", "TypeScript"]
   },
   {
